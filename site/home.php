cfb98e3ebead58deff9cf2ccb81fd814df1c1ed9
ngocbq3 <bqngoc@gmail.com>
2019-10-12 16:31:54 +0700

    update productdetail

--- site/home.php
+++ site/home.php
@@ -6,7 +6,7 @@ $products = hang_hoa_per_page(0, 8);
 <div class="product">
     <?php foreach ($products as $p) { ?>
         <div class="items">
-            <a href="#">
+            <a href="?action=productdetail&ma_hh=<?=$p['ma_hh']?>">
                 <h3><?=$p["ten_hh"]?></h3>
                 <img src="content/images/products/<?=$p['hinh']?>" alt="">
                 <p>
--- site/productdetail.php
+++ site/productdetail.php
@@ -1,1 +1,18 @@
 <h1>Product Detail</h1>
+<?php
+    if (isset($_GET['ma_hh'])) {
+        $ma_hh = $_GET['ma_hh'];
+        $row = hang_hoa_list_one($ma_hh);
+?>
+    <div class="row">
+        <h3><?=$row['ten_hh']?></h3>
+        <img src="content/images/products/<?=$row['hinh']?>" alt="">
+        <h3>Đơn giá: <?=$row['don_gia']?></h3>
+        <h3>Giảm giá: <?=$row['giam_gia']?></h3>
+        <p>
+            <?=$row['mo_ta']?>
+        </p>
+    </div>
+<?php
+    }
+?>
